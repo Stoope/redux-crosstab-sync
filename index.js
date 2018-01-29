0f9acf2242fb22f912b7aa3608fdcad15338c08f
Stepan Kurennykh <s.kurennykh@e-mba.ru>
2018-01-29 13:52:15 +0300

    es6 to es5

--- index.js
+++ index.js
@@ -1,39 +1,56 @@
-let lastTimeStamp = 0;
+"use strict";
 
-const timestampAction = action => {
-  const stampedAction = action;
+var lastTimeStamp = 0;
+
+var timestampAction = function timestampAction(action) {
+  var stampedAction = action;
   stampedAction.$time = Date.now();
   return {
-    stampedAction
+    stampedAction: stampedAction
   };
 };
 
-const actionStorageMiddleware = () => next => action => {
-  if (action && !action.$time) {
-    try {
-      const stampedAction = timestampAction(action);
-      lastTimeStamp = stampedAction.$time;
-      localStorage.setItem("LAST_ACTION", JSON.stringify(stampedAction));
-    } catch (e) {}
-  }
-  return next(action);
+var actionStorageMiddleware = function actionStorageMiddleware() {
+  return function(next) {
+    return function(action) {
+      if (action && !action.$time) {
+        try {
+          var stampedAction = timestampAction(action);
+          lastTimeStamp = stampedAction.$time;
+          localStorage.setItem("LAST_ACTION", JSON.stringify(stampedAction));
+        } catch (e) {}
+      }
+      return next(action);
+    };
+  };
 };
 
-const createStorageListener = (store, config = {}) => {
-  const exclude = config.exclude;
-  const include = config.include;
+var createStorageListener = function createStorageListener(store) {
+  var config =
+    arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-  let checkActionType = () => false;
+  var exclude = config.exclude;
+  var include = config.include;
+
+  var checkActionType = function checkActionType() {
+    return false;
+  };
 
   if (exclude) {
-    checkActionType = type => !exclude.includes(type);
+    checkActionType = function checkActionType(type) {
+      return !exclude.includes(type);
+    };
   } else if (include) {
-    checkActionType = type => include.includes(type);
+    checkActionType = function checkActionType(type) {
+      return include.includes(type);
+    };
   }
 
-  window.addEventListener("storage", event => {
+  window.addEventListener("storage", function(event) {
     try {
-      const { stampedAction } = JSON.parse(event.newValue) || {};
+      var _ref = JSON.parse(event.newValue) || {},
+        stampedAction = _ref.stampedAction;
+
       if (
         stampedAction &&
         stampedAction.$time !== lastTimeStamp &&
@@ -46,4 +63,7 @@ const createStorageListener = (store, config = {}) => {
   });
 };
 
-module.exports = { actionStorageMiddleware, createStorageListener };
+module.exports = {
+  actionStorageMiddleware: actionStorageMiddleware,
+  createStorageListener: createStorageListener
+};
